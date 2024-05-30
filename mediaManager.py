c9672ca95d372f59d365d364061e70f5bde3ac8e
Andrej Vyšný <vysnyandrej@gmail.com>
2024-05-31 01:33:23 +0200

    subtitles

--- mediaManager.py
+++ mediaManager.py
@@ -10,6 +10,34 @@ import re
 import string
 import shutil
 from unidecode import unidecode
+from pymediainfo import MediaInfo
+
+def check_video_file(file_path):
+    media_info = MediaInfo.parse(file_path)
+    audio_tracks = []
+    subtitle_tracks= []
+
+    for track in media_info.tracks:
+        if track.track_type == "Text":
+            subtitle_track_info = {
+                "Language": track.language if track.language else "Unknown",
+                "Format": track.format
+            }
+            subtitle_tracks.append(subtitle_track_info)
+        if track.track_type == "Audio":
+            audio_track_info = {
+                "Language": track.language if track.language else "Unknown",
+                "Channel(s)": track.channel_s,
+                "Sampling Rate": track.sampling_rate,
+                "Format": track.format
+            }
+            audio_tracks.append(audio_track_info)
+
+    return audio_tracks, subtitle_tracks
+
+
+
+
 
 def sanitize_folder_name(name):
     """
@@ -55,6 +83,13 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Web and Form Application')
         self.selectedFilePath = ""
 
+        self.lngExtDict = {
+            "ENtit":"en",
+            "SKtit":"sk",
+            "CZtit":"cz",
+        }
+
+        self.selectSub= {}
         self.resize(1920, 1080)  # Set the window size to 800x600 pixels
 
         self.mainWidget = QWidget()
@@ -82,6 +117,10 @@ class MainWindow(QMainWindow):
         self.playButton.setStyleSheet("background-color : green")
         self.playButton.clicked.connect(self.play_file)
 
+        # Media Info
+        self.media_audio = QLabel(f"Audio: null")
+        self.media_text = QLabel(f"Sub: null")
+
         # Checkboxes section
         checkboxLayout = QVBoxLayout()
         self.audioGroup = self.createCheckboxGroup("Audio", ["EN", "SK", "CZ"], checkboxLayout)
@@ -92,11 +131,30 @@ class MainWindow(QMainWindow):
         self.submitButton.setStyleSheet("background-color : orange")
         self.submitButton.clicked.connect(self.submit_form)
 
+        fileLayout.addWidget(self.media_audio)
+        fileLayout.addWidget(self.media_text)
+        fileLayout.addWidget(self.playButton)
+
+        self.subSelected = {}
+
+        self.sub= {}
+        self.subLabel = {}
+
+        self.subtitlesLayout = QVBoxLayout()
+
+        for langTit in ["ENtit", "SKtit", "CZtit"]:
+            self.create_sub_select(langTit)
+        
+
         # Assembling form layout
         formLayout.addLayout(fileLayout)
-        formLayout.addWidget(self.playButton)
+        
         formLayout.addLayout(checkboxLayout)
+
+        formLayout.addLayout(self.subtitlesLayout)
         formLayout.addWidget(self.submitButton)
+
+
 
         splitter = QSplitter(Qt.Vertical)
         splitter.addWidget(self.webView)
@@ -104,16 +162,54 @@ class MainWindow(QMainWindow):
 
         layout.addWidget(splitter)
 
-    def createCheckboxGroup(self, title, options, layout):
+    def createCheckboxGroup(self, title, options, layout, select = False):
         group = QGroupBox(title)
         groupLayout = QVBoxLayout()
         for option in options:
+
+
+            # TODO not working correctly 
+            if select:
+                self.selectSub[option] = QPushButton("Sub")
+                self.selectSub[option].clicked.connect(self.choose_file)
+                groupLayout.addWidget(self.selectSub[option])
+
             checkBox = QCheckBox(option)
             groupLayout.addWidget(checkBox)
             setattr(self, f"checkBox{option}", checkBox)
         group.setLayout(groupLayout)
         layout.addWidget(group)
         return group
+    
+
+    def create_sub_select(self, lang):
+
+        self.subLabel[lang] = QLabel(f"{lang} No file selected")
+        self.subLabel[lang].setWordWrap(True)
+
+        self.sub[lang] = QPushButton(f"Select {lang}")
+        self.sub[lang].clicked.connect(lambda: self.choose_sub(lang))
+
+        self.subtitlesLayout.addWidget(self.subLabel[lang])
+        self.subtitlesLayout.addWidget(self.sub[lang])
+
+    def choose_sub(self, lang):            # TODO not working correctly 
+                # Define the initial directory relative to the current working directory
+        initialDir = os.path.join(os.getcwd(), "inputs")
+        
+        # Check if the directory exists, and create it if it doesn't
+        if not os.path.exists(initialDir):
+            os.makedirs(initialDir)
+
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(self, "Select TIT File", initialDir, "All Files (*);;Python Files (*.py)", options=options)
+
+        print(self.subSelected)
+        if fileName:
+            self.subSelected[lang] = fileName
+            self.subLabel[lang].setText(os.path.basename(fileName))
+            print("\n\tSelected subtitle: "+fileName)
+            print(self.subSelected)
 
     def choose_file(self):
         # Define the initial directory relative to the current working directory
@@ -128,6 +224,13 @@ class MainWindow(QMainWindow):
         if fileName:
             self.selectedFilePath = fileName
             self.fileNameLabel.setText(os.path.basename(fileName))
+
+            audio_tracks, subtitle_tracks = check_video_file(fileName)
+
+            self.media_audio.setText("Audio\n"+"\n".join(str(d) for d in audio_tracks))
+            self.media_text.setText("Subtitles\n"+"\n".join(str(d) for d in subtitle_tracks))
+        
+            print("\n\n####################################\n\n")
 
     def submit_form(self):
         audioSelections = [checkBox.text() for checkBox in self.audioGroup.findChildren(QCheckBox) if checkBox.isChecked()]
@@ -166,6 +269,15 @@ class MainWindow(QMainWindow):
         # Move and rename the file
         shutil.move(self.selectedFilePath, targetFilePath)
 
+        for lang in self.subSelected: 
+
+            filePath = self.subSelected[lang]
+            fileNameWithoutExtension, fileExtension = os.path.splitext(filePath)
+
+            targetTitFilePath =  f"{folderName} - {selectedOptions}.{str(self.lngExtDict[lang])}{fileExtension}"
+            shutil.move(filePath, os.path.join(targetDir, targetTitFilePath))
+
+
         print(f"File moved and renamed to: {targetFilePath}")
 
         self.reset_form()
@@ -173,6 +285,13 @@ class MainWindow(QMainWindow):
     def reset_form(self):
         self.webView.setUrl(QUrl(self.TMDB_URL))
         self.fileNameLabel.setText("No file selected")
+        self.media_audio.setText("No Audio")
+        self.media_text.setText("No Subtitles")
+        
+        for langTit in ["ENtit", "SKtit", "CZtit"]:
+            self.subLabel[langTit].setText(f"No {langTit} file selected")
+
+        self.subSelected = {}
         self.selectedFilePath = ""
         for checkBox in self.audioGroup.findChildren(QCheckBox) + self.subtitleGroup.findChildren(QCheckBox):
             checkBox.setChecked(False)
